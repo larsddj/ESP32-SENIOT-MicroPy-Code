ba7f09ca7114dac2904e5da978036cfc53a4c674
Lars <lars.d.j@hotmail.com>
2021-12-28 14:58:20 +0100

    fixed timing of measurements, inserted sensor code for microphone

--- sensors.py
+++ sensors.py
@@ -2,30 +2,36 @@ import time
 from machine import Pin, ADC
 from measurements import MQMeasurement, MicrophoneMeasurement, HumidTempMeasurement
 
+mqSensorPin = 39 # the GPIO pin the MQ-135 sensor is plugged into on the ESP32
+micSensorPin = 36 # the GPIO pin the microphone sensor is plugged into on the ESP32
+
 # these sensor methods retrieve measurements from a sensor and attach a timestamp object
 # these measurements and timestamps are used for JSON serialization in createMeasurementJson.py
 
 def get_microphone_measurements():
-    clockStart = time.time()
-    mic = MicrophoneMeasurement(100.34)
-    clockEnd = time.time()
-    timeSpentMs = clockEnd-clockStart
+    clockStart = time.time_ns()
+    micSensor = ADC(Pin(micSensorPin))         
+    micSensor.atten(ADC.ATTN_11DB)   #This sets the maximum range of the sensor to 3.3v which is the input limit of the ESP32
+
+    micReading = MQMeasurement(mqSensor.read())
+    clockEnd = time.time_ns()
+    timeSpentMs = (clockEnd-clockStart)/1000000
     
-    mic.setTimeSpent(timeSpentMs) 
-    return mic
+    micReading.setTimeSpent(timeSpentMs) 
+    return micReading
 
 def get_mq_measurements():
-    clockStart = time.time()
-    mqSensor = ADC(Pin(39))         #The GPIO pin the sensor is plugged into on the ESP32
+    clockStart = time.time_ns()
+    mqSensor = ADC(Pin(mqSensorPin))         
     mqSensor.atten(ADC.ATTN_11DB)   #This sets the maximum range of the sensor to 3.3v which is the input limit of the ESP32
 
-    mq = MQMeasurement(mqSensor.read())
+    mqReading = MQMeasurement(mqSensor.read())
 
-    clockEnd = time.time()
-    timeSpentMs = clockEnd-clockStart
-    mq.setTimeSpent(timeSpentMs)
+    clockEnd = time.time_ns()
+    timeSpentMs = (clockEnd-clockStart)/1000000
+    mqReading.setTimeSpent(timeSpentMs)
 
-    return mq
+    return mqReading
 
 def get_humTemp_measurements():
     clockStart = time.time()
