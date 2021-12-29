27374edd1f551d33b66ddcf5774bd41ccb853fe5
Lars <lars.d.j@hotmail.com>
2021-12-29 17:53:59 +0100

    expanded createMeasurementJson for microphone data

--- createMeasurementJson.py
+++ createMeasurementJson.py
@@ -1,14 +1,17 @@
-def createJson():
+def createMQJson():
     import sensors as s
     import json as j
 
     returnableJson = ""
     # measure measurement and dump the object structure into usable JSON
     returnableJson = j.dumps(s.get_mq_measurements().__dict__)
-    """print("the json returned stored in returnableJson:")
-    print(returnableJson)
-    print("the object in returnablejson:")
+    return returnableJson
 
-    print("--checking for JSON validity w/ jloads--")
-    j.loads(returnableJson) """
+def createMicrophoneJson():
+    import sensors as s
+    import json as j
+
+    returnableJson = ""
+    # measure measurement and dump the object structure into usable JSON
+    returnableJson = j.dumps(s.get_microphone_measurements().__dict__)
     return returnableJson
