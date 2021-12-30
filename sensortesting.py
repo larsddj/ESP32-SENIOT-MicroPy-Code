96981d3ccfdf044791394bacc81115dfd96b2f96
Lars <lars.d.j@hotmail.com>
2021-12-30 20:18:32 +0100

    we now use digital out on the sound detecting module
    for detecting above 85dB with hardware sided calibration we now use a digital value instead of the previously used analog value

--- sensortesting.py
+++ sensortesting.py
@@ -3,14 +3,13 @@ from machine import Pin, ADC
 from measurements import MQMeasurement, MicrophoneMeasurement, HumidTempMeasurement
 
 mqSensorPin = 39 # the GPIO pin the MQ-135 sensor is plugged into on the ESP32
-micSensorPin = 36 # the GPIO pin the microphone sensor is plugged into on the ESP32
+micSensorPin = 34 # the GPIO pin the microphone sensor is plugged into on the ESP32
 
 # these sensor methods retrieve measurements from a sensor and attach a timestamp object
 # these measurements and timestamps are used for JSON serialization in createMeasurementJson.py
 
 def get_microphone_measurements():
-    micSensor = ADC(Pin(micSensorPin))         
-    micSensor.atten(ADC.ATTN_11DB)   #This sets the maximum range of the sensor to 3.3v which is the input limit of the ESP32
+    micSensor = Pin(micSensorPin, Pin.IN)         
 
     while(1):
         micReading = MicrophoneMeasurement(micSensor.read())
