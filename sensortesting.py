97715c4c4a1793ec3e69b821701dc82569841103
Lars <lars.d.j@hotmail.com>
2022-01-25 21:10:34 +0100

    code for DHT11 testing

--- sensortesting.py
+++ sensortesting.py
@@ -1,8 +1,10 @@
 import time
+from time import sleep
 from machine import Pin, ADC
+import dht 
 from measurements import MQMeasurement, MicrophoneMeasurement, HumidTempMeasurement
 
-mqSensorPin = 39 # the GPIO pin the MQ-135 sensor is plugged into on the ESP32
+mqSensorPin = 18 # the GPIO pin the MQ-135 sensor is plugged into on the ESP32
 micSensorPin = 34 # the GPIO pin the microphone sensor is plugged into on the ESP32
 
 # these sensor methods retrieve measurements from a sensor and attach a timestamp object
@@ -23,3 +25,39 @@ def get_mq_measurements():
     while(1):
         mqReading = MQMeasurement(mqSensor.read())
         print(mqReading.airQuality)
+
+#testing method for the dht11 sensor
+def get_dht11_measurements():
+    sensor = dht.DHT11(Pin(14))
+
+    while(1):
+        sleep(2)
+        sensor.measure()
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+        temp_f = temp * (9/5) + 32.0
+        print('Temperature: %3.1f C' %temp)
+        print('Temperature: %3.1f F' %temp_f)
+        print('Humidity: %3.1f %%' %hum)
+
+#method to compare multiple dht11's for calibration purposes
+def compare_dht11_measurements():
+    sensor = dht.DHT11(Pin(14))
+    sensor2 = dht.DHT11(Pin(27))
+
+    while(1):
+        sleep(2)
+        sensor.measure()
+        sensor2.measure()
+        temp = sensor.temperature()
+        temp2 = sensor2.temperature()
+        hum = sensor.humidity()
+        hum2 = sensor2.humidity()
+        temp_f = temp * (9/5) + 32.0
+        temp_f2 = temp2 * (9/5) + 32.0
+        print('Temperature: %3.1f C' %temp)
+        print('Temperature: %3.1f F' %temp_f)
+        print('Humidity: %3.1f %%' %hum)
+        print('Temperature 2: %3.1f C' %temp2)
+        print('Temperature 2: %3.1f F' %temp_f2)
+        print('Humidity 2: %3.1f %%' %hum2)
