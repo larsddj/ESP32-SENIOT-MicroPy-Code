e42fe0b0d6a83ef74cd91c814c99e7162d25c33c
Lars <lars.d.j@hotmail.com>
2022-01-28 17:46:48 +0100

    post requests for dht11

--- httpRequests.py
+++ httpRequests.py
@@ -2,6 +2,15 @@ def http_post():
     import urequests
     import createMeasurementJson
 
+    # first we access the endpoint for DHT11 sensordata
+    url = "https://isensiot-api.herokuapp.com/api/createhumtemp"
+    headers = {'content-type': 'application/json'}
+    # measure measurements and create JSON from it
+    data = createMeasurementJson.createHumTempJson()
+    print(data)
+    response = urequests.post(url, data=data, headers=headers)
+    response.close()
+
     # then we access the endpoint for the microphone sensordata
     url = "https://isensiot-api.herokuapp.com/api/createmicrophone"
     headers = {'content-type': 'application/json'}
@@ -12,14 +21,17 @@ def http_post():
     response = urequests.post(url, data=data, headers=headers)
     response.close()
 
-    # first we access the endpoint for MQ sensordata
-    url = "https://isensiot-api.herokuapp.com/api/createmq135"
-    headers = {'content-type': 'application/json'}
-    # measure measurements and create JSON from it
-    data = createMeasurementJson.createMQJson()
-    print(data)
-    response = urequests.post(url, data=data, headers=headers)
-    response.close()
+
+
+
+    # # first we access the endpoint for MQ sensordata
+    # url = "https://isensiot-api.herokuapp.com/api/createmq135"
+    # headers = {'content-type': 'application/json'}
+    # # measure measurements and create JSON from it
+    # data = createMeasurementJson.createMQJson()
+    # print(data)
+    # response = urequests.post(url, data=data, headers=headers)
+    # response.close()
 
 
 
