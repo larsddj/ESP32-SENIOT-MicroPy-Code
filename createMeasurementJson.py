363e1350649a9f80dd814aae57fa25d1da44ea15
Lars <lars.d.j@hotmail.com>
2022-01-28 17:46:35 +0100

    added json creation for both dht11 and sound module

--- createMeasurementJson.py
+++ createMeasurementJson.py
@@ -13,5 +13,14 @@ def createMicrophoneJson():
 
     returnableJson = ""
     # measure measurement and dump the object structure into usable JSON
-    returnableJson = j.dumps(s.get_microphone_measurements().__dict__)
+    returnableJson = j.dumps(s.get_microphone_measurements_avg().__dict__)
+    return returnableJson
+
+def createHumTempJson():
+    import sensors as s
+    import json as j
+
+    returnableJson = ""
+    # measure measurement and dump the object structure into usable JSON
+    returnableJson = j.dumps(s.get_humTemp_measurements().__dict__)
     return returnableJson
