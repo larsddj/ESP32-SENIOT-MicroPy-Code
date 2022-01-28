6c1b0726f525bc5c7135b8ebde8423104997fc3f
Lars <lars.d.j@hotmail.com>
2022-01-28 17:49:44 +0100

    fixed the frequent OSErrors from the dht11
    our code ran too quick for the 1hz refresh rate of the dht11, it simply was not ready when it needed to be.
    we poll it now like earlier, if it errors out we wait and try again in 5 seconds.
    this happens 5 times.

--- sensors.py
+++ sensors.py
@@ -1,9 +1,18 @@
 import time
+from time import sleep
 from machine import Pin, ADC
+import dht
 from measurements import MQMeasurement, MicrophoneMeasurement, HumidTempMeasurement
 
 mqSensorPin = 39 # the GPIO pin the MQ-135 sensor is plugged into on the ESP32
 micSensorPin = 34 # the GPIO pin the microphone sensor is plugged into on the ESP32
+dhtSensorPin = 4 # the GPIO pin the DHT11 sensor is plugged into on the ESP32
+
+dhtErrorIterations = 5 # the amount of times a DHT11 measurement will be attempted before giving up
+
+# we draw multiple samples and average the digital samples out to a single number, mic_cutoff_value refers to this
+mic_cutoff_value = 0.95 # the cutoff value at which we determine sound pollution has occured
+mic_sample_amount = 5000 # the amount of digital microphone values we poll
 
 # these sensor methods retrieve measurements from a sensor and attach a timestamp object
 # these measurements and timestamps are used for JSON serialization in createMeasurementJson.py
@@ -13,9 +22,9 @@ def get_microphone_measurements():
     micSensor = Pin(micSensorPin, Pin.IN)         
 
     micReading = MicrophoneMeasurement(micSensor.read())
+    
     clockEnd = time.time_ns()
     timeSpentMs = (clockEnd-clockStart)/1000000
-    
     micReading.setTimeSpent(timeSpentMs) 
     return micReading
 
@@ -63,10 +72,18 @@ def get_mq_measurements():
     return mqReading
 
 def get_humTemp_measurements():
-    clockStart = time.time()
-    humTemp = HumidTempMeasurement(94, 33.45)
-    clockEnd = time.time()
-    timeSpentMs = clockEnd-clockStart
+    print ("we got into the sensor yay")
+    sensor = dht.DHT11(Pin(4))
     
-    humTemp.setTimeSpent(timeSpentMs)
-    return humTemp
+    i = 0
+    while(i < dhtErrorIterations):
+        try:
+            sensor.measure()
+            temp = sensor.temperature()
+            hum = sensor.humidity()
+            dhtreading = HumidTempMeasurement(hum, temp)
+            return dhtreading
+        except OSError:
+            print("There was a problem connecting to the DHT11... retrying in 5 seconds...")
+            sleep(5)
+            i+=1
